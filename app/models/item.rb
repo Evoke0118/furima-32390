aa3dab0f0837ce31d6e81e6d05c2757cc972b0a6
Evoke0118 <evoke.baddvoltage@gmail.com>
2021-04-04 12:21:09 +0900

    バリデーションの記述まで

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,31 @@
 class Item < ApplicationRecord
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :image
+  belongs_to :user
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :shipping_date
+
+
+  validates :images, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1 }, presence: true
+  validates :status_id, numericality: { other_than: 1 }, presence: true
+  validates :postage_id, numericality: { other_than: 1 }, presence: true
+  validates :region_id, numericality: { other_than: 1 }, presence: true
+  validates :shipping_date__id, numericality: { other_than: 1 }, presence: true
+  VALID_PRICEL_HALF = /\A[0-9]+\z/ 
+  validates :price, presence: true,
+                    numericality: { with: VALID_PRICEL_HALF, message: 'Half-width number' }
+  validates :price, numericality: {
+      greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+      message: 'Out of setting range' } 
+
+
 end
+
+
