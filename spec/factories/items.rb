f8399130727bb2358118cd77e68a46331be0a1ba
Evoke0118 <evoke.baddvoltage@gmail.com>
2021-04-09 15:25:30 +0900

    モデルのテストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    image             {}
+    name              {"オムライス"}
+    description       {"あああああ"}
+    category_id       {2}
+    status_id         {2}
+    postage_id        {2}
+    region_id         {2}
+    shipping_date_id  {2}
+    price             {300}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,9 +1,75 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
-  describe "ユーザー新規登録" do
-    it "nicknameが空だと登録できない" do
+    before do
+      @item = FactoryBot.build(:item)
     end
-    it "emailが空では登録できない" do
+
+    context '出品できないとき' do
+      it "imageが空では登録できないこと" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
     end
-  end
+
+    it "nameが空では登録できないこと" do
+      @item.name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end     
+
+    it "descriptionが空では登録できないこと" do
+      @item.description = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
+    end  
+
+    it "category_idが空では登録できないこと" do
+      @item.category_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")
+    end  
+
+    it "status_idが空では登録できないこと" do
+      @item.status_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status can't be blank")
+    end 
+
+    it "postage_idが空では登録できないこと" do
+      @item.status_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status can't be blank")
+    end 
+
+    it "region_idが空では登録できないこと" do
+      @item.region_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Region can't be blank")
+    end 
+
+    it "shipping_date_idが空では登録できないこと" do
+      @item.shipping_date_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping date can't be blank")
+    end 
+
+    it "priceが空では登録できないこと" do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end 
+
+    it "価格の範囲が、¥300~¥9,999,999の間でないと登録できないこと" do
+      @item.price = 10
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Out of setting range")
+    end 
+
+    it "販売価格は半角数字でないと保存できないこと" do
+      @item.price = "ああああ"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Half-width number")
+    end
+
 end
