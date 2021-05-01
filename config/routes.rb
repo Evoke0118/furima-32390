91bd6fdb07f0222474c8a3e12017042dd7288853
Evoke0118 <evoke.baddvoltage@gmail.com>
2021-05-01 14:20:36 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "items#index"
-  resources :items, only: [:index, :new, :create, ]
+  # devise_for :addresses
+  # devise_for :orders
+  # devise_for :users
+  # root to: "items#index"
+  # resources :items, only: [:index, :new]
 end
